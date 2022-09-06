532fbc267f1a3bd8fd3a61c91e0ed7d1be26f7e3
LinYiMing <13975073750@163.com>
2022-09-06 11:17:32 +0800

    -feat修改app.vue的注释

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// git log 查看 commit 记录
+// git result 回退到某个 commit 的版本
+// gei status 查看当前仓库各个文件的状态
+// git add . 提交到暂存区
+// git commit -m "-feat项目初始化"
+// - feat：新增特性
+// - style：样式的修改
+// - fixed：debug
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
